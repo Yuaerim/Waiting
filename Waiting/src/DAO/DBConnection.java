9ceb9b6824a2de93b4dbdb7bba8e09beeebf76d4
rkdhs <rkdhs@DESKTOP-JINF6PF>
2022-01-24 17:51:37 +0900

    aer

---
+++ Waiting/src/DAO/DBConnection.java
@@ -0,0 +1,24 @@
+package DAO;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBConnection {
+   static Connection conn=null;
+   
+   public static Connection connect() {
+      try {   
+         Context initContext = new InitialContext(); 
+         String path="java:/comp/env/";
+         String dePath="jdbc/mysql";
+         DataSource ds = (DataSource) initContext.lookup(path+dePath);
+         conn = ds.getConnection();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      return conn;
+   }
+}
--- Waiting/src/DAO/UserDAO.java
+++ Waiting/src/DAO/UserDAO.java
@@ -1,5 +1,10 @@
 package DAO;
 
+import java.sql.Connection;
+
 public class UserDAO {
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	
 
 }
